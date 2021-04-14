ac31c3b8769898a6906525862fd48af388af580d
Hiroko Yamaji <hiroko@hirokoymj.com>
2021-04-14 21:25:23 +0900

    Deleted unused folders

--- Math/Done/generate-random.js
+++
@@ -1,6 +0,0 @@
-function getRandomArbitrary(min, max) {
-  //return Math.random() * (max - min) + min;
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-console.log(getRandomArbitrary(5,8));
